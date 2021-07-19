a314dcbc264778c2b9483ed21051465388d5fd38
remoteSwin <swin223@163.com>
2021-07-19 14:15:54 +0800

    更新新的章节的一些代码

---
+++ GdbStudy/6-CoreFile/CoreErrorTest.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    char *Msg = "Hello World!";
+    Msg[0] = '1';
+    return 0;
+}
---
+++ LinuxServer/10-Signal/10-4/UnifiedEvent.cpp
@@ -0,0 +1,19 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/epoll.h>
+#include <pthread.h>
+
+#define MAX_EVENT_NUMBER 1024
+static int pipefd[2];
+
+
