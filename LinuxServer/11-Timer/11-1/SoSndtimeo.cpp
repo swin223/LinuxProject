bf053f973934d2cfe5852888d75864e80164d774
remoteSwin <swin223@163.com>
2021-07-20 13:59:29 +0800

    add timer example

---
+++ LinuxServer/11-Timer/11-1/SoSndtimeo.cpp
@@ -0,0 +1,69 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+int timeout_connect(const char* ip,int port,int time)
+{
+    // 根据ip和port来创建对端的socket地址
+    int ret = 0;
+    struct sockaddr_in address;
+    bzero(&address,sizeof(address));
+    address.sin_family = AF_INET;
+    inet_pton(AF_INET,ip,&address.sin_addr);
+    address.sin_port = htons(port);
+
+    // 创建本地的socket
+    int sockfd = socket(PF_INET,SOCK_STREAM,0);
+    assert(sockfd >= 0);
+
+    // 设置socket的选项
+    // 设置的超时时间类型是timeval
+    struct timeval timeout;
+    timeout.tv_sec = time;
+    timeout.tv_usec = 0;
+    socklen_t len = sizeof(timeout);
+    ret = setsockopt(sockfd,SOL_SOCKET,SO_SNDTIMEO,&timeout,len);
+    assert(ret != -1);
+
+    // 进行connect连接
+    ret = connect(sockfd,(struct sockaddr*)&address,sizeof(address));
+    if(ret == -1)
+    {
+        // 超时对应的错误号是EINPROGRESS,如果下面这个条件成立的话，我们就可以处理定时任务了
+        if(errno == EINPROGRESS)
+        {
+            printf("connecting timeout,process timeout logic\n");
+            return -1;
+        }
+        printf("error occur when connecting to server\n");
+        return -1;
+    }
+    printf("connect success!");
+    return sockfd;
+}
+
+int main(int argc,char* argv[])
+{
+    if(argc <= 2)
+    {
+        printf("parameter is not enough!");
+    }
+    const char* ip = argv[1];
+    int port = atoi(argv[2]);
+
+    int sockfd = timeout_connect(ip,port,10);
+    if(sockfd < 0)
+    {
+        return 1;
+    }
+
+    return 0;
+}
---
+++ LinuxServer/5-SocketAPI/5-8/sendMsgOob.c
@@ -0,0 +1,46 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    if(argc <= 2)
+    {
+        printf("parameters is error!\n");
+        return 1;
+    }
+    const char *ip = argv[1];
+    int port = atoi(argv[2]);
+
+    struct sockaddr_in server_address;
+    bzero(&server_address,sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    inet_pton(AF_INET,ip,&server_address.sin_addr);
+    server_address.sin_port = htons(port);
+
+    int sockfd = socket(PF_INET,SOCK_STREAM,0);
+    assert(sockfd >= 0);
+    if(connect(sockfd,(struct sockaddr*)&server_address,sizeof(server_address)) < 0)
+    {
+        printf("failed!");
+    }
+    else
+    {
+        const char* oob_data = "this is oob data\n";
+        const char* normal_data = "this is normal data\n";
+        send(sockfd,normal_data,strlen(normal_data),0);
+        send(sockfd,oob_data,strlen(oob_data),MSG_OOB);
+        send(sockfd,normal_data,strlen(normal_data),0);
+        send(sockfd,normal_data,strlen(normal_data),0);
+        send(sockfd,oob_data,strlen(oob_data),MSG_OOB);
+        send(sockfd,normal_data,strlen(normal_data),0);
+    }
+
+    close(sockfd);
+    return 0;
+}
